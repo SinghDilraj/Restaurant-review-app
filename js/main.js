d09210d507aec062fc1e54d755d8da9c998dea95
Dilraj Singh <dilrajkehra@gmail.com>
2019-01-07 09:26:09 -0600

    upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(reg => {
+        console.log('service worker is registered', reg)
+      })
+      .catch(err => {
+        console.log('service worker is not registered', err)
+      })
+  })
+}
+
 let restaurants,
   neighborhoods,
   cuisines
